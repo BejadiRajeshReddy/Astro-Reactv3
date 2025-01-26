95362ca03f525c5de23833a79288f51b3477e771
RajeshReddy <bejadirajeshreddy@gmail.com>
2025-01-26 07:31:42 +0530

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainPage from "./components/Main";
-import SignUp from "./components/Signup";
-import Login from "./components/Login";
-import Horoscope from "./components/Horo";
+import SignUp from "./pages/Signup";
+import Login from "./pages/Login";
+import Horoscope from "./pages/Horo";
 
 function App() {
   return (
