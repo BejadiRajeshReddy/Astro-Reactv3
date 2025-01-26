236974d1f552c27ccb044bf0bc634753ac34aa0d
RajeshReddy <bejadirajeshreddy@gmail.com>
2025-01-26 14:20:44 +0530

    removed extra lines

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,6 @@ import Hero from "./Hero";
 import Services from "./Services"
 import Doshas from "./Doshas"
 import About from "./About"
-// import LoginPage from "./Login";
 
 const MainPage = () => (
     <>
